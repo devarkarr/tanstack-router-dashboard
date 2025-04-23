0f30944258f8f04b8bf71b3b3ad358e014d22ac6
arkarlin2003 <arkarlin486@gmail.com>
2025-04-23 16:19:07 +0630

    feat(app): table filters

--- src/components/organisms/data-table.tsx
+++ src/components/organisms/data-table.tsx
@@ -5,6 +5,7 @@ import {
   ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFacetedRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
@@ -23,6 +24,30 @@ import {
 } from "@/components/atoms/table";
 import React from "react";
 import { Button } from "../atoms/button";
+import { Input } from "../atoms/input";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "../atoms/dropdown-menu";
+import {
+  ArrowUpDown,
+  ChevronsUpDown,
+  CircleHelp,
+  CirclePlus,
+  Settings2,
+} from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "../atoms/popover";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../atoms/command";
+import { Checkbox } from "../atoms/checkbox";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -49,6 +74,7 @@ export function DataTable<TData, TValue>({
     getSortedRowModel: getSortedRowModel(),
     onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
     onRowSelectionChange: setRowSelection,
     onColumnVisibilityChange: setColumnVisibility,
     getPaginationRowModel: getPaginationRowModel(),
@@ -60,8 +86,127 @@ export function DataTable<TData, TValue>({
     },
   });
 
+  const categoryColumn = table.getColumn("category");
+  const facetedCategories = categoryColumn?.getFacetedUniqueValues();
+  console.log(Array.from(facetedCategories?.entries() || []));
+  
   return (
     <div className="p-4">
+      <div className="flex items-center py-4 gap-2">
+        <div className="flex items-center gap-3">
+          <Input
+            placeholder="Search titles..."
+            value={table.getColumn("title")?.getFilterValue() as string}
+            onChange={(event) =>
+              table.getColumn("title")?.setFilterValue(event.target.value)
+            }
+            className="max-w-lg"
+          />
+
+          <Input
+            placeholder="Filter emails..."
+            value={
+              (table.getColumn("category")?.getFilterValue() as string) ?? ""
+            }
+            onChange={(event) =>
+              table.getColumn("category")?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm"
+          />
+          <Popover>
+            <PopoverTrigger>
+              <Button variant="outline" className="gap-1.5">
+                <CirclePlus className="h-4 w-4" />
+                Categories
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent
+              align="start"
+              side="bottom"
+              className="p-0 rounded-sm max-w-52"
+            >
+              <Command className="rounded-sm  shadow-md w-full">
+                <CommandInput placeholder="Categories" />
+                <CommandList>
+                  <CommandEmpty>No results found.</CommandEmpty>
+                  <CommandGroup>
+                    <CommandItem>
+                      <Checkbox />
+                      <div className="w-full flex items-center justify-between text-gray-600 dark:text-gray-200">
+                        <p className="flex items-center gap-1.5 ">
+                          <CircleHelp />
+                          <span>Todo</span>
+                        </p>
+                        <p>8</p>
+                      </div>
+                    </CommandItem>
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="ml-auto space-x-2 flex items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="gap-1.5">
+                <ArrowUpDown className="h-4 w-4" />
+                Sort
+                <p className="bg-slate-200 dark:bg-slate-800 px-2 py-0.5 rounded text-xs">
+                  1
+                </p>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  );
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="gap-1.5">
+                <Settings2 className="h-4 w-4" />
+                View
+                <ChevronsUpDown className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  );
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
       <div className="rounded-md border ">
         <Table>
           <TableHeader>
@@ -90,7 +235,7 @@ export function DataTable<TData, TValue>({
                   data-state={row.getIsSelected() && "selected"}
                 >
                   {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
+                    <TableCell key={cell.id} className="py-2.5">
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
--- src/pages/products/components/columns.tsx
+++ src/pages/products/components/columns.tsx
@@ -2,8 +2,21 @@
 
 import { Button } from "@/components/atoms/button";
 import { Checkbox } from "@/components/atoms/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/atoms/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown } from "lucide-react";
+import {  Ellipsis,ChevronsUpDown } from "lucide-react";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -43,24 +56,57 @@ export const columns: ColumnDef<Product>[] = [
   },
   {
     accessorKey: "title",
-    header: "Title",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Title
+        <ChevronsUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => <p className=" line-clamp-1">{row.getValue("title")}</p>,
   },
   {
     accessorKey: "category",
-    header: "Category",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Category
+        <ChevronsUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    meta:{
+      filterVariant: 'select',
+    }
+  },
+  {
+    accessorKey: "rating.rate",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Rating
+        <ChevronsUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => {
+      return <p className="text-center">{row.original.rating.rate}</p>;
+    },
   },
   {
     accessorKey: "price",
     header: ({ column }) => (
-      <div className="flex justify-end">
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Price
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      </div>
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Price
+        <ChevronsUpDown className="ml-2 h-4 w-4" />
+      </Button>
     ),
     cell: ({ row }) => {
       const amount = parseFloat(row.getValue("price"));
@@ -69,7 +115,41 @@ export const columns: ColumnDef<Product>[] = [
         currency: "USD",
       }).format(amount);
 
-      return <div className="text-right font-medium">{formatted}</div>;
+      return <div className="text-center font-medium">{formatted}</div>;
     },
+  },
+  {
+    accessorKey: "",
+    header: " ",
+    cell: () => (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="ms-5">
+          <button className="p-1 hover:bg-slate-200 hover:dark:bg-gray-800 rounded focus-visible:ring-0 focus-visible:outline-0">
+            <Ellipsis size={17} />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40 rounded-sm">
+          <DropdownMenuGroup>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger>Labels</DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent className="rounded-sm">
+                  <DropdownMenuItem>Email</DropdownMenuItem>
+                  <DropdownMenuItem>Message</DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>More...</DropdownMenuItem>
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            Log out
+            <DropdownMenuShortcut>⇧⌘D</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    ),
   },
 ];
